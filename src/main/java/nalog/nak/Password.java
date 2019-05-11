ab666cf442a6a48748083f18a57446f1a307148b
17130899 <49346834+17130899@users.noreply.github.com>
2019-05-11 10:47:11 +0500

    Update Password.java

--- src/main/java/nalog/nak/Password.java
+++ src/main/java/nalog/nak/Password.java
@@ -1,6 +1,6 @@
-package nalog.nak; 
+package NalogC; 
 /* Импортируем:
-javax.* — стандартные расширения.(Стандартные расширения (standard extensions) — это пакеты или набо-ры пакетов Java)
+javax.* — стандартные расширения. (Стандартные расширения (standard extensions) — это пакеты или наборы пакетов Java)
 Swing - Библиотека графического интерфейса (конкретный пакет), В отличие от java.awt, javax.swing имеет более гибкую
 систему управления и более широкий набор функций
 "*"-включение всех элементов библиотеки Swing (jbutton,jpanel,jlabel и т.д.)
@@ -36,8 +36,8 @@ passwordField = new JPasswordField(10);
 
 passwordField.setActionCommand(OK); 
 passwordField.addActionListener(this); 
-/*Обозначаем что переменные label
- *  являются объектом JLabel и указываем какой текст будет выводиться.
+/*Обозначаем, что переменные label
+ *  являются объектом JLabel, и указываем, какой текст будет выводиться.
 */
 JLabel label = new JLabel("Введите пароль: "); 
 /*Создание кнопок*/
@@ -54,16 +54,16 @@ add(buttonPane);
 /*Модификатор protected — переменные, методы и конструкторы, которые объявляются как protected в классе,
  *  могут быть доступны только для подклассов в другом пакете или для любого класса в пакете класса protected.*/
 protected JComponent createButtonPanel() { 
-	/* установка менеджера компоновки GridLayout. данный менеджер компоновки располагает компоненты в
+	/* установка менеджера компоновки GridLayout. Данный менеджер компоновки располагает компоненты в
 	таблицу */
 	/* на панели 0 строк, 1 столбец*/
 JPanel p = new JPanel(new GridLayout(0,1)); 
-/*Обозначаем что переменные okButton,helpButton  являются объектом
+/*Обозначаем, что переменные okButton, helpButton  являются объектом
  *  JButton
  *  В скобках наименование кнопок */
 JButton okButton = new JButton("OK"); 
 JButton helpButton = new JButton("Help"); 
-/*Передаётся в переменные okButton,helpButton строковое значение */
+/*Передаётся в переменные okButton, helpButton строковое значение */
 okButton.setActionCommand(OK); 
 helpButton.setActionCommand(HELP); 
 okButton.addActionListener(this); 
@@ -81,7 +81,7 @@ String cmd = e.getActionCommand();
 if (OK.equals(cmd)) { 
 char[] input = passwordField.getPassword();
 /*Если выполнится условие, то будет показано окно диалога
- * с текстом "Пароль правильный" затем произойдёт вызов main1 из класса Nalog*/
+ * с текстом "Пароль правильный", затем произойдёт вызов main1 из класса Nalog*/
 if (isPasswordCorrect(input)) { 
 JOptionPane.showMessageDialog(controllingFrame, 
 "Пароль правильный"); 
@@ -89,7 +89,7 @@ Nalog.main1(null);
 /*Закрытие диалогового окна*/
 controllingFrame.dispose(); 
 } else { 
-	/*Если же будет ошибка, то вызовится диалоговое окно(ошибки) с названием
+	/*Если же будет ошибка, то вызовется диалоговое окно (ошибки) с названием
 	 * "Ошибка" и содержимым "Пароль не верный"*/
 JOptionPane.showMessageDialog(controllingFrame, 
 "Пароль не верный", 
@@ -100,7 +100,7 @@ JOptionPane.ERROR_MESSAGE);
 Arrays.fill(input, '0'); 
 passwordField.selectAll(); 
 resetFocus(); 
-/*Если не нажали на кнопку "ОК", а на "Help", то вызовится диалоговое окно в котором будет
+/*Если не нажали на кнопку "ОК", а на "Help", то вызовется диалоговое окно в котором будет
  * пароль "admin"*/
 } else { 
 JOptionPane.showMessageDialog(controllingFrame, 
@@ -110,10 +110,10 @@ JOptionPane.showMessageDialog(controllingFrame,
 /*boolean логический тип*/
 private static boolean isPasswordCorrect(char[] input) { 
 boolean isCorrect = true; 
-/*Проверка на правильность введёного пароля*/
+/*Проверка на правильность введённого пароля*/
 char[] correctPassword = { 'a', 'd', 'm', 'i', 'n'}; 
 /*Если введённый пароль не совпадает с переменной correctPassword,
- * то выдаст сообщение об ошибке в противном случае выдаст что пароль был введён 
+ * то выдаст сообщение об ошибке в противном случае выдаст, что пароль был введён 
  * правильно */
 if (input.length != correctPassword.length) { 
 isCorrect = false; 
@@ -154,12 +154,3 @@ createAndShowGUI();
 }); 
 } 
 }
-
-
-
-
-
-
-
-
-
